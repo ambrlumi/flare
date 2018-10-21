b84b750a2f868f24833619f73b1c72649ad840b0
carloslahrssen <carloslahrssen@gmail.com>
2018-10-21 07:56:54 -0400

    new url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  BLAZE_URL: "https://a4a3223c.ngrok.io/api",
+  BLAZE_URL: "http://664fc7a6.ngrok.io",
   MAP_TOKEN:
     "pk.eyJ1IjoiZGNhc3QxODgiLCJhIjoiY2pmNjE4cW5sMTVlbDJ4cDRuaXg2bGdocCJ9._vCZx3elzygB4Alr55WMYw"
 };
