ac1237f003978f5e86ea5a29b80525665916a1dc
Juan Alvarado <jalvarado91@gmail.com>
2018-10-21 08:58:45 -0400

    bruh

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  BLAZE_URL: "http://84157a38.ngrok.io/api",
+  BLAZE_URL: "https://84157a38.ngrok.io/api",
   MAP_TOKEN:
     "pk.eyJ1IjoiZGNhc3QxODgiLCJhIjoiY2pmNjE4cW5sMTVlbDJ4cDRuaXg2bGdocCJ9._vCZx3elzygB4Alr55WMYw"
 };
