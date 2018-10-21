7f07907dc7b417288e450f7d382ab3f70de893dc
carloslahrssen <carloslahrssen@gmail.com>
2018-10-20 20:52:44 -0400

    added functional map with marker tracking

--- src/components/MapGL.js
+++ src/components/MapGL.js
@@ -3,14 +3,6 @@ import ReactMapGL, { Marker } from "react-map-gl";
 
 import config from "../config";
 
-const viewport = {
-  width: 400,
-  height: 400,
-  latitude: 37.7577,
-  longitude: -122.4376,
-  zoom: 8
-};
-
 export default class MapGL extends React.Component {
   state = {
     cases: [
@@ -22,8 +14,8 @@ export default class MapGL extends React.Component {
         tag: "XXX234"
       },
       {
-        lat: "81.2380",
-        lng: "-25.4250",
+        lat: "25.7684",
+        lng: "-80.3671",
         make: "VW",
         color: "Blue",
         tag: "XXX234"
@@ -32,44 +24,47 @@ export default class MapGL extends React.Component {
     viewport: {
       width: 100,
       height: 100,
-      latitude: 25.7574,
-      longitude: -80.3733,
-      zoom: 8
+      latitude: 0,
+      longitude: 0,
+      zoom: 14
     }
   };
 
-  componentDidMount() {
-    const { cases } = this.props;
-    const mapContainer = document.querySelector(".map");
+  componentDidMount = async () => {
+    const { latitude, longitude } = await this.getCoords();
+    const { viewport } = this.state;
+    this.setState({ viewport: { ...viewport, latitude, longitude } });
 
+    const mapContainer = document.querySelector(".map");
     const { width, height } = mapContainer.getClientRects()[0];
 
-    this.setState(({ cases, viewport }) => {
-      return { cases, viewport: { ...viewport, width: width, height: height } };
+    this.setState(({ cases, viewport }) => ({
+      cases,
+      viewport: { ...viewport, width: width, height: height }
+    }));
+  };
+
+  getCoords = () =>
+    new Promise(resolve => {
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        resolve(coords);
+      });
     });
-  }
+
+  markers = cases =>
+    cases.map(({ lat, lng }, i) => (
+      <Marker key={i} latitude={parseFloat(lat)} longitude={parseFloat(lng)}>
+        <img src={require("../static/images/marker.svg")} alt="marker" />
+      </Marker>
+    ));
 
   render() {
-    const markers = this.state.cases.map(({ lat, lng }, keys) => {
-      return (
-        <Marker
-          key={keys}
-          latitude={parseFloat(lat)}
-          longitude={parseFloat(lng)}
-        >
-          <img />
-        </Marker>
-      );
-    });
+    const { cases, viewport } = this.state;
 
     return (
       <div className="map">
-        <ReactMapGL
-          {...this.state.viewport}
-          mapboxApiAccessToken={config.MAP_TOKEN}
-          onViewportChange={viewport => this.setState({ viewport })}
-        >
-          {markers}
+        <ReactMapGL {...viewport} mapboxApiAccessToken={config.MAP_TOKEN}>
+          {this.markers(cases)}
         </ReactMapGL>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,15 @@ import React from "react";
 export default class Navbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar expand-lg navbar-light bg-light">
+      <nav className="navbar expand-lg">
         {/* <span className="navbar-brand mb-0 h1">Ambr</span> */}
         <span className="navbar-brand">
-          <img src="../static/images/icon.png" width="30" height="30" alt="" />
+          <img
+            src={require("../static/images/icon.png")}
+            width="50"
+            height="50"
+            alt=""
+          />
         </span>
       </nav>
     );
